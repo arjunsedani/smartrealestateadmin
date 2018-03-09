a2a44f4ba57748aa0e3b4a3f934fa831b32e420b
arjunsedani <sedaniarjun@gmail.com>
2018-03-09 19:55:16 +0530

    Update app.js

--- public/app.js
+++ public/app.js
@@ -1,9 +1,7 @@
-// configs
 var apiUrl = '/'
 
 // get DOM element references
 var floorPlanEl = document.querySelector('#floor-plan-url')
-var floorPlanE2 = document.querySelector('#floor-plan-url2')
 var addressEl = document.querySelector('#address')
 var emailEl = document.querySelector('#email')
 var buttonEl = document.querySelector('.convert-button')
@@ -11,8 +9,6 @@ var apiInfoEl = document.querySelector('#api-info')
 var buttonClear = document.querySelector('#btn-clear')
 var dropArea = document.querySelector('.dropzone-area')
 var dropZone = document.querySelector('.dropzone-upload')
-var dropArea2 = document.querySelector('.dropzone2-area')
-var dropZone2 = document.querySelector('.dropzone2-upload')
 
 // create file drop box
 io3d.utils.ui.fileDrop({
@@ -27,20 +23,6 @@ io3d.utils.ui.fileDrop({
     }
   }
 })
-// create file drop box2
-io3d.utils.ui.fileDrop({
-  elementId: 'file-drop-box2',
-  upload: true,
-  dragOverCssClass: 'file-drop-box-dragover',
-  // set file url if upload succeded
-  onInput: function (files) {
-    if (files && files.length) {
-      floorPlanE2.value = files[0].url2
-      dropZone2.src = files[0].url2
-    }
-  }
-})
-
 // check for manual file url input
 floorPlanEl.addEventListener('input', function(evt){
   var url = evt.target.value
@@ -55,20 +37,6 @@ floorPlanEl.addEventListener('input', function(evt){
   } else clearImg()
   console.log(url)
 })
-// check for manual file url input2
-floorPlanE2.addEventListener('input', function(evt){
-  var url2 = evt.target.value
-  var isValid = /(http(s?):)|([/|.|\w|\s])*\.(?:jpg|gif|png)/.test(url)
-  if (url2 && url2 !== '') {
-    if (!isValid) {
-      floorPlanE2.style.color = 'red'
-      console.log('invalid')
-      return
-    }
-    dropZone2.src = url2
-  } else clearImg()
-  console.log(url2)
-})
 // show img element if loading succeeded
 dropZone.onload = function() {
   dropArea.style.display = 'none'
@@ -81,31 +49,17 @@ dropZone.onerror = function() {
   floorPlanEl.style.color = 'red'
   clearImg()
 }
-dropZone2.onload = function() {
-  dropArea2.style.display = 'none'
-  dropZone2.style.display = 'block'
-  floorPlanE2.style.color = '#27292b'
-  dropZone.setAttribute('data-valid', true)
-}
-// handle image loading errors
-dropZone2.onerror = function() {
-  floorPlanE2.style.color = 'red'
-  clearImg()
-}
 // reset the upload box
 function clearImg() {
   dropZone.removeAttribute('data-valid')
   dropArea.style.display = 'block'
   dropZone.style.display = 'none'
-    dropArea2.style.display = 'block'
-  dropZone2.style.display = 'none'
 }
 // clear entire form
 buttonClear.addEventListener('click', function(){
   emailEl.value=''
   addressEl.value=''
   floorPlanEl.value=''
-  floorPlanE2.value=''
   apiInfoEl.innerHTML=''
   clearImg()
 })
@@ -116,7 +70,7 @@ function submitHandler() {
   if (!dropZone.getAttribute('data-valid')) {
     io3d.utils.ui.message('image is not valid')
     return false;
-  }
+}
   // start API request
   //apiInfoEl.innerHTML = 'Sending API request...<br>'
   apiInfoEl.innerHTML = 'Publishing to our App<br>'
